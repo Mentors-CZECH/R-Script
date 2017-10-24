d69faf873dcaf5a322f8c4a62f9aa1f8f2bd3b91
Lubomír Rýgl <lubomir.rygl@gmail.com>
2017-10-24 21:24:38 +0200

    Update projektu

---
+++ 001_Základy.R
@@ -0,0 +1,101 @@
+
+
+
+
+
+#F5 Sestavení celého projektu
+#CTRL+Enter - Proede øádek na kterém se nachází kurzor
+
+
+12 + 13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Pøiøazení hodnoty k promìnné
+myVariable <- 13.5
+a
+### ELEMENTY ###
+
+#class umožní urèit typ elementu
+class("Lorem ipsum")
+
+#Logické hodnoty
+class(TRUE)
+
+#Je dùležité že R používá jako oddìlovaè desetinných míst "." namísto v èesku známého ","
+class(13.5)
+
+#L urèuje že se jedná o integer
+class(2L)
+
+#Komplexní èísla
+class(2+5i)
+
+#Znaky (string / Character)
+class("Text")
+
+#Raw
+class(charToRaw("Text"))
+
+
+
+
+#######################################################################################
+
+#Vektor je skupina elemtù
+Ovoce <- c("Pomeranè","Jablko","Meloun","Hruška","Lièi","Banán")
+print(Ovoce)
+
+
+#List
+SeznamPolozek <- list(c(2,3,5,3,7,9),25.99,c("Poznámka_1","Poznámka_2"))
+SeznamPolozek
+
+#Matice prvkù
+Matice <- matrix(c("a", "b", "c", "d", "a","c"), nrow = 2, ncol = 3, byrow = TRUE)
+Matice
+
+#Pole
+Pole <- array(c("a", "v", "d"), dim = c(3,2,3))
+Pole
+
+#Faktor
+Ovoce <- c("Pomeranè", "Jablko", "Meloun", "Hruška", "Lièi", "Banán", "Jablko", "Jablko", "Pomeranè")
+Faktor_Ovoce <- factor(Ovoce)
+Faktor_Ovoce
+#Poèet faktorù v promìnné
+nlevels(Faktor_Ovoce)
+
+
+#Datové rámce (Data Frames)
+BMI <- data.frame(
+                      pohlaví = c("Muž","Žena","Muž","Žena","Muž","Muž"),
+                      výška = c(189, 171, 181, 180, 168, 195),
+                      Váha = c(98,55,87,48,120,95),
+                      Vìk = c(26,20,24,18,35,58)
+                 )
+BMI
